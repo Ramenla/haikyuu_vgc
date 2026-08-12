24e39c20041de2461e8c05bf5348d0f2cb445adb
lanaff <2211102218@ittelkom-pwt.ac.id>
2026-08-12 14:05:15 +0700

    chore: menghapus file script dan catatan sementara yang tidak digunakan

--- check_names.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const db = fs.readFileSync('src/data/cardDatabase.ts', 'utf8');
-const regex = /id:\s*[\`\"'](HVD-[^\`\"']+)[\`\"'],\s*name:\s*[\`\"']([^\`\"']+)[\`\"']/g;
-let m;
-while((m = regex.exec(db)) !== null) {
-  console.log(m[1] + ' : ' + m[2]);
-}
